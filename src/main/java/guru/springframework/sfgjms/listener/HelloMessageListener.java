5dc98a11cd7a63de1d7f768b92c614cf5c972e05
Jose Esquinca <esquinca_jose@hotmail.com>
2022-05-02 17:51:06 -0500

    receive-jms-msg

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+        System.out.println("I got a message!!!!!");
+
+        System.out.println(helloWorldMessage);
+        
+        // uncomment and view to see retry count in debugger
+        // throw new RuntimeException("foo");
+    }
+}
